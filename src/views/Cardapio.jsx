8985ada38c058a20cd9b846f85ceaf0e16d1fdb0
Wagner Gonçalves <wagner.goncalves@calindra.com.br>
2021-04-30 17:36:59 -0300

    exibindo as pizzas no front

--- src/views/Cardapio.jsx
+++ src/views/Cardapio.jsx
@@ -8,19 +8,23 @@ class Cardapio extends React.Component {
       disk_pizza: [],
       refrigerantes: [],
       pizzas: [],
+      precos: [],
     };
   }
 
   buscaPizza = async () => {
     const response = await api.get("");
     const pizzas = response.data.pizzas;
+    const precos = response.data.precos;
     const refrigerantes = response.data.refrigerantes;
 
     this.setState({
       disk_pizza: response.data,
       pizzas: pizzas,
+      precos: precos,
       refrigerantes: refrigerantes,
     });
+    console.log("tes", response.data)
 
   /*   pizzas[0].tradicionais[0].sabores.map((piz) => {
       return <h2>{console.log("asa",piz.nome, piz.codigo_pizza)}</h2>
@@ -38,11 +42,20 @@ class Cardapio extends React.Component {
       <div>
         <h1>Cardapio</h1>
 
+        <h2>Pizzas</h2>
+        {this.state.pizzas.map((pizza) => {
+          return (
+            <h3 key={pizza.codigo_pizza}>
+               {pizza.nome} 
+            </h3>
+          );
+        })}
+
         <h2>Refrigerante Grátis</h2>
         {this.state.refrigerantes.map((refri) => {
           return (
             <h3 key={refri.codigo_refri}>
-              Tamanho : {refri.tamanho} - litros: {refri.litro}
+              Tamanho : {refri.tamanho} -  {refri.litro} litros:
             </h3>
           );
         })}
