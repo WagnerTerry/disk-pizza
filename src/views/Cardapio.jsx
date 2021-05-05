cda1ae20dc5fd37dff32bd9e6b5b179568ff566f
WagnerTerry <wagner.goncalves@calindra.com.br>
2021-05-05 12:13:10 -0300

    retornando o nome das pizzas

--- src/views/Cardapio.jsx
+++ src/views/Cardapio.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import api from "../services/api";
 
+import "./Cardapio.scss";
+
 class Cardapio extends React.Component {
   constructor() {
     super();
@@ -8,29 +10,27 @@ class Cardapio extends React.Component {
       disk_pizza: [],
       refrigerantes: [],
       pizzas: [],
-      precos: [],
+      grupos: [],
     };
   }
 
   buscaPizza = async () => {
     const response = await api.get("");
     const pizzas = response.data.pizzas;
-    const precos = response.data.precos;
+    const grupos = response.data.grupos;
     const refrigerantes = response.data.refrigerantes;
 
     this.setState({
       disk_pizza: response.data,
       pizzas: pizzas,
-      precos: precos,
+      grupos: grupos,
       refrigerantes: refrigerantes,
     });
-    console.log("tes", response.data)
+    console.log("tes", response.data);
 
-  /*   pizzas[0].tradicionais[0].sabores.map((piz) => {
+    /*   pizzas[0].tradicionais[0].sabores.map((piz) => {
       return <h2>{console.log("asa",piz.nome, piz.codigo_pizza)}</h2>
     }) */
-
-  
   };
 
   componentDidMount = () => {
@@ -38,16 +38,32 @@ class Cardapio extends React.Component {
   };
 
   render() {
+    // const grupos = this.state.pizzas.filter((g) => g.grupo === "ESPECIAIS");
+    const gr = this.state.grupos[0];
+    console.log("s", gr);
     return (
-      <div>
+      <div id={"cardapio"}>
         <h1>Cardapio</h1>
-
         <h2>Pizzas</h2>
+
+        <h2>Tradicionais </h2>
         {this.state.pizzas.map((pizza) => {
           return (
-            <h3 key={pizza.codigo_pizza}>
-               {pizza.nome} 
-            </h3>
+            pizza.grupo === "TRADICIONAIS" && (
+              <div key={pizza.codigo_pizza}>
+                <h3>{pizza.nome}</h3>
+              </div>
+            )
+          );
+        })}
+        <h2>Classicas</h2>
+        {this.state.pizzas.map((pizza) => {
+          return (
+            pizza.grupo === "CLASSICAS" && (
+              <div key={pizza.codigo_pizza}>
+                <h3>{pizza.nome}</h3>
+              </div>
+            )
           );
         })}
 
@@ -55,7 +71,7 @@ class Cardapio extends React.Component {
         {this.state.refrigerantes.map((refri) => {
           return (
             <h3 key={refri.codigo_refri}>
-              Tamanho : {refri.tamanho} -  {refri.litro} litros:
+              Tamanho : {refri.tamanho} - {refri.litro} litros:
             </h3>
           );
         })}
