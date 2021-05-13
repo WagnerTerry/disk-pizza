bf232511225fa17c2c65e4edebbd1bca55971307
WagnerTerry <wagner.goncalves@calindra.com.br>
2021-05-13 18:15:13 -0300

    testando formik com hooks

--- src/views/CadastroCliente.jsx
+++ src/views/CadastroCliente.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+// import { Formik } from "formik";
 import "./CadastroCliente.scss";
 
-// const [values, setValues] = useState({
-//   name: "",
-// });
-
-class CadastroCliente extends React.Component {
+function CadastroCliente() {
   // constructor() {
   //   super();
   // }
 
-  onBlurCep = (e) => {
+  const [values, setValues] = useState({
+    name: "",
+  });
+
+  function onBlurCep(e, setFieldValue) {
     const { value } = e.target;
 
     const cep = value?.replace(/[^0-9]/g, "");
@@ -22,79 +23,86 @@ class CadastroCliente extends React.Component {
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
       .then((res) => res.json())
       .then((data) => console.log(data));
-  };
+  }
 
-  render() {
-    return (
-      <div id="cadastro-cliente">
-        <h2>Cadastro de Clientes</h2>
+  function onChange(e) {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-        <header>
-          <div>
-            <label htmlFor="cod-cliente">Código do Cliente: </label>
-            <input type="text" id="cod-cliente" name="cod-cliente" size="5" />
-          </div>
-          <div>
-            {" "}
-            <button>Buscar</button>
-            <button>Limpar</button>
-            <button>Salvar</button>
-            <button>Alterar</button>
-            <button>Apagar</button>
-          </div>
-          <div></div>
-          {/* <br />
+  return (
+    <div id="cadastro-cliente">
+      <h2>Cadastro de Clientes</h2>
+
+      <header>
+        <div>
+          <label htmlFor="cod-cliente">Código do Cliente: </label>
+          <input type="text" id="cod-cliente" name="cod-cliente" size="5" />
+        </div>
+        <div>
+          {" "}
+          <button>Buscar</button>
+          <button>Limpar</button>
+          <button>Salvar</button>
+          <button>Alterar</button>
+          <button>Apagar</button>
+        </div>
+        <div></div>
+        {/* <br />
           <br /> */}
-        </header>
+      </header>
 
-        <main>
-          <form action="/">
+      <main>
+        <form action="/">
+          <div>
             <div>
-              <div>
-                <label htmlFor="nome-cliente">Nome: </label>
-                <input
-                  type="text"
-                  id="nome-cliente"
-                  name="nome-cliente"
-                  size="50"
-                />
-              </div>
-              <div>
-                <label htmlFor="telefone">Telefone: </label>
-                <input type="number" id="telefone" name="telefone" />
-              </div>
-            </div>
-
-            <div>
-              <label htmlFor="cep">Cep: </label>
+              <label htmlFor="name">Nome: </label>
               <input
                 type="text"
-                id="cep"
-                name="cep"
-                maxLength="9"
-                onBlur={this.onBlurCep}
+                id="name"
+                name="name"
+                size="50"
+                value={values.name}
+                onChange={onChange}
               />
             </div>
             <div>
-              <label htmlFor="logradouro">Logradouro: </label>
-              <input type="text" id="logradouro" name="logradouro" size="50" />
+              <label htmlFor="telefone">Telefone: </label>
+              <input type="number" id="telefone" name="telefone" />
             </div>
+          </div>
 
-            <div>
-              <label htmlFor="bairro">Bairro: </label>
-              <input type="text" id="bairro" name="bairro" size="40" />
-              <label htmlFor="localidade">Cidade: </label>
-              <input type="text" id="localidade" name="localidade" size="40" />
-            </div>
-            <div>
-              <label htmlFor="obs">Observações: </label>
-              <textarea name="obs" cols="50" rows="3"></textarea>
-            </div>
-          </form>
-        </main>
-      </div>
-    );
-  }
+          <div>
+            <label htmlFor="cep">Cep: </label>
+            <input
+              type="text"
+              id="cep"
+              name="cep"
+              maxLength="9"
+              onBlur={onBlurCep}
+            />
+          </div>
+          <div>
+            <label htmlFor="logradouro">Logradouro: </label>
+            <input type="text" id="logradouro" name="logradouro" size="50" />
+          </div>
+
+          <div>
+            <label htmlFor="bairro">Bairro: </label>
+            <input type="text" id="bairro" name="bairro" size="40" />
+            <label htmlFor="localidade">Cidade: </label>
+            <input type="text" id="localidade" name="localidade" size="40" />
+          </div>
+          <div>
+            <label htmlFor="obs">Observações: </label>
+            <textarea name="obs" cols="50" rows="3"></textarea>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
 
 export default CadastroCliente;
